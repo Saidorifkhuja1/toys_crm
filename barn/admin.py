546285ff80ef93ea5606076bd1fa8ee7fbc307ad
said <saidorifx@gmail.com>
2025-08-04 16:36:58 +0500

    toys

--- barn/admin.py
+++ barn/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Barn
 
-# Register your models here.
+
+
+@admin.register(Barn)
+class BarnAdmin(admin.ModelAdmin):
+    list_display = ['name']
