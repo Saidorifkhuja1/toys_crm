1cc05f0f5bbc005aa72c77ae0202a612495fda9e
said <saidorifx@gmail.com>
2025-08-09 10:46:19 +0500

    toys

--- config/settings.py
+++ config/settings.py
@@ -69,6 +69,8 @@ REST_FRAMEWORK = {
         "rest_framework.filters.SearchFilter",
     ],
     "DEFAULT_LIST_SERIALIZER_CLASS": "core.serializers.LimitedListSerializer",
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
 }
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(hours=2),
